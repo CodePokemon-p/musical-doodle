e5c9bf643a76916bc5a32df95320bc6da4ac102e
CodePokemon-p <sabamin@07gmail.com>
2024-02-25 11:02:28 +0500

    csharp file

---
+++ Hey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+        }
+    }
+}
